5b84b0d81669e4aeb09f3e476498bf922c08318a
Felix Patawah <devfep@gmail.com>
2022-10-16 11:58:27 -0400

    Add entities and dtos

---
+++ proj4-noteApp/src/main/java/com/devfep/proj4noteApp/Proj4NoteAppApplication.java
@@ -0,0 +1,13 @@
+package com.devfep.proj4noteApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj4NoteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj4NoteAppApplication.class, args);
+	}
+
+}
---
+++ proj4-noteApp/src/test/java/com/devfep/proj4noteApp/Proj4NoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfep.proj4noteApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj4NoteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
